dc81b687112a96953f75051930760ac3aa675c51
kaderchambi <146840025+kaderchambi@users.noreply.github.com>
2023-11-30 10:57:32 +0100

    git add

---
+++ main_q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	// Affichage du message d'accueil
+	printf("Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n");
+
+	// Boucle pour le prompt
+	while (1) {
+    	printf("enseash %% "); // Prompt
+
+    	// Lire la commande de l'utilisateur
+    	char command[100]; // Définissez la taille en fonction de vos besoins
+    	fgets(command, sizeof(command), stdin);
+
+    	// Supprimez le saut de ligne à la fin de la commande
+    	command[strcspn(command, "\n")] = '\0';
+
+    	// Vérifiez si l'utilisateur a saisi 'exit'
+    	if (strcmp(command, "exit") == 0) {
+        	printf("Au revoir !\n");
+        	break; // Quittez la boucle
+    	}
+
+    	// Ici, vous traiterez l'exécution de la commande (étape suivante)
+	}
+
+	return 0;
+}
+
